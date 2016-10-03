8af305712173cc79074ceefda89cc8e38e6b6216
TJ2001 <jung.m.tim@gmail.com>
2016-10-02 18:57:05 -0700

    add button header for easy navigation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -87,6 +87,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/sightings", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      // model.put("sightings", sightings.all());
+      model.put("template", "templates/sightings.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
 //so far done
 
